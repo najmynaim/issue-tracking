0b0ea6dcaaca70f853b105c9d64978a9d3a618d8
Your Name <you@example.com>
2023-12-25 19:56:21 +0800

    Build an api for creating issue

---
+++ prisma/migrations/20231225103036_create_issue/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Issue` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
